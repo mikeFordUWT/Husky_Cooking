c54e71ae5a9139bd2cc51103723147c5c9df2d44
IanSkyles <ianskyles@gmail.com>
2016-05-02 16:49:14 -0700

    Removed unused imports in UserHomeFragment.java

--- app/src/main/java/team14/tacoma/uw/edu/husky_cooking/UserHomeFragment.java
+++ app/src/main/java/team14/tacoma/uw/edu/husky_cooking/UserHomeFragment.java
@@ -6,7 +6,6 @@
 package team14.tacoma.uw.edu.husky_cooking;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -14,7 +13,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 
 
 /**
