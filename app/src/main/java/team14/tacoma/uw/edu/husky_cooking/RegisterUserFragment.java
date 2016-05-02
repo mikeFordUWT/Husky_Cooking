2af1fdaace4115f40a3e6a4743a4f4d688ae901b
michaelford <fordm13@uw.edu>
2016-05-01 17:21:05 -0700

    progress commit

---
+++ app/src/main/java/team14/tacoma/uw/edu/husky_cooking/RegisterUserFragment.java
@@ -0,0 +1,29 @@
+package team14.tacoma.uw.edu.husky_cooking;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegisterUserFragment extends Fragment {
+
+
+    public RegisterUserFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register_user, container, false);
+    }
+
+}
