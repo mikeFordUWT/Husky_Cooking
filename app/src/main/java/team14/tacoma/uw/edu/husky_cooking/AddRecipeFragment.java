0788e3a812ab92e24fbd499050c627a25064da75
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-05-02 13:01:16 -0700

    Time to comment

--- app/src/main/java/team14/tacoma/uw/edu/husky_cooking/AddRecipeFragment.java
+++ app/src/main/java/team14/tacoma/uw/edu/husky_cooking/AddRecipeFragment.java
@@ -1,22 +1,34 @@
 package team14.tacoma.uw.edu.husky_cooking;
 
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AddRecipeFragment extends Fragment {
+    private static final ArrayList<Integer> COOK_TIMES
+            = new ArrayList<Integer>(Arrays.asList(5,10,15,20,25,30,35,40, 45,50,55,60,65,70,75,80
+                ,85,90,95,100,105,110,115,120));
+    private static final ArrayList<Integer> SERVINGS
+            = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
+    private EditText mRecipeName, mRecipeDescript, mCookingTime;
+    private Spinner mCookTimeSpin, mServingsSpin;
+    private List<Integer> mCookTimes, mServings;
 
 
 
@@ -30,10 +42,34 @@ public class AddRecipeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mCookTimes = new ArrayList<Integer>();
+        mServings = new ArrayList<Integer>();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_recipe, container, false);
+        View v = inflater.inflate(R.layout.fragment_add_recipe, container, false);
+        mRecipeName = (EditText) v.findViewById(R.id.recipe_name);
+        mRecipeDescript = (EditText) v.findViewById(R.id.recipe_description);
+        mCookTimeSpin = (Spinner) v.findViewById(R.id.cook_time_spinner);
+        mServingsSpin = (Spinner) v.findViewById(R.id.servings_spinner);
+
+        Button addRecipeButton = (Button) v.findViewById(R.id.add_recipe_button);
+
+        addRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String recipeName = mRecipeName.getText().toString();
+                String recipeDescript = mRecipeDescript.getText().toString();
+            }
+        });
     }
 
+    private void populateCooktimeSpinner(){
+
+    }
+
+    private void populateServingSpinner(){
+
+    }
 
 
 }
--- app/src/main/java/team14/tacoma/uw/edu/husky_cooking/authenticate/SignInActivity.java
+++ app/src/main/java/team14/tacoma/uw/edu/husky_cooking/authenticate/SignInActivity.java
@@ -8,8 +8,6 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -199,7 +197,7 @@ public class SignInActivity extends AppCompatActivity
     }
 
 
-    class DownloadUserTask extends AsyncTask<String, Void, String> {
+    class LoginTask extends AsyncTask<String, Void, String> {
         boolean failure= false;
 
         @Override
