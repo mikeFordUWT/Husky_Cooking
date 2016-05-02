1f11020dda84717434b8986d03a36aa7f693c973
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-05-02 13:02:38 -0700

    Time to comment

--- app/src/main/java/team14/tacoma/uw/edu/husky_cooking/AddRecipeFragment.java
+++ app/src/main/java/team14/tacoma/uw/edu/husky_cooking/AddRecipeFragment.java
@@ -28,7 +28,6 @@ public class AddRecipeFragment extends Fragment {
 
     private EditText mRecipeName, mRecipeDescript, mCookingTime;
     private Spinner mCookTimeSpin, mServingsSpin;
-    private List<Integer> mCookTimes, mServings;
 
 
 
@@ -42,8 +41,7 @@ public class AddRecipeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mCookTimes = new ArrayList<Integer>();
-        mServings = new ArrayList<Integer>();
+
 
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_add_recipe, container, false);
