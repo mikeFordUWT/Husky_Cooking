308da1f7e0b46302ac14ab6e79c8735db1b7c512
IanSkyles <ianskyles@gmail.com>
2016-05-02 14:24:47 -0700

    Commented User.java

--- app/src/main/java/team14/tacoma/uw/edu/husky_cooking/authenticate/User.java
+++ app/src/main/java/team14/tacoma/uw/edu/husky_cooking/authenticate/User.java
@@ -1,3 +1,8 @@
+/*
+ * Mike Ford and Ian Skyles
+ * TCSS450 – Spring 2016
+ * Recipe Project
+ */
 package team14.tacoma.uw.edu.husky_cooking.authenticate;
 
 import org.json.JSONArray;
@@ -11,15 +16,30 @@ import java.util.List;
 import team14.tacoma.uw.edu.husky_cooking.model.Recipe;
 
 /**
- * Created by michaelford on 4/30/16.
+ * This class was designed to model a User of the app.
+ * Users will consist of the id, email, and cook book.
+ * @author Mike Ford
+ * @author Ian Skyles
+ * @version 5/2/2016
  */
 public class User implements Serializable{
+    /** Constant strings representing user id and email.  */
     public static final String ID = "user_id", EMAIL = "email";
 
+    /** A integer which will store the users (unique, autoincrementing) id number. */
     private int mId;
+    /** A String which will store the users's email. */
     private String mEmail;
+    /** An ArrayList that stores all the recipes in a users personalized cookbook. */
     private ArrayList<Recipe> mCookBook;
 
+    /**
+     * This constructor creates a new User with
+     * specified id and email. It also creates an empty cookbook.
+     *
+     * @param id The id of the ingredient.
+     * @param email The name of the ingredient.
+     */
     public User(int id, String email){
         this.mId = id;
         this.mEmail = email;
@@ -28,22 +48,38 @@ public class User implements Serializable{
     }
 
 
+    /**
+     * Returns the users (unique) id number.
+     * @return user id
+     */
     public int getId() {
         return mId;
     }
 
 
-
+    /**
+     * Returns the users email address.
+     * @return user email
+     */
     public String getEmail() {
         return mEmail;
     }
 
 
-
+    /**
+     * Returns the users personalized list of recipes (cookbook).
+     * @return lists of recipes
+     */
     public ArrayList<Recipe> getCookBook() {
         return mCookBook;
     }
 
+    /**
+     * Works with Serializable to parse a json item.
+     * @param userJSON Javascript object representing user
+     * @param userList A list of users
+     * @return null if successfully parsed JSON or the reason it failed as a string
+     */
     public static String parseUserJSON(String userJSON, List<User> userList){
         String reason = null;
         if(userJSON !=null){
@@ -63,7 +99,11 @@ public class User implements Serializable{
     }
 
 
-
+    /**
+     * Turns the user into a string representation which
+     * only consists of email at this point. (We might add a lists of recipes later)
+     * @return user's email
+     */
     @Override
     public String toString() {
         return this.getEmail();
