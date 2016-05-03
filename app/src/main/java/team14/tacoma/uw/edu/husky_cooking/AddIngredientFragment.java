9ccd17e84950138641edf2eacc459cc7b302dca9
IanSkyles <ianskyles@gmail.com>
2016-05-03 15:11:46 -0700

    Added Java Doc to AddIngredientFragment.java

--- app/src/main/java/team14/tacoma/uw/edu/husky_cooking/AddIngredientFragment.java
+++ app/src/main/java/team14/tacoma/uw/edu/husky_cooking/AddIngredientFragment.java
@@ -1,3 +1,9 @@
+/*
+ * Mike Ford and Ian Skyles
+ * TCSS450 – Spring 2016
+ * Recipe Project
+ */
+
 package team14.tacoma.uw.edu.husky_cooking;
 
 
@@ -9,16 +15,30 @@ import android.view.ViewGroup;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * This class controls the user adding ingredients to a recipe.
+ * It interfaces with our database hosted on CSSGATE.
+ *
+ * @author Mike Ford
+ * @author Ian Skyles
+ * @version 5/2/2016
  */
 public class AddIngredientFragment extends Fragment {
 
-
+    /**
+     * Required empty public constructor
+     */
     public AddIngredientFragment() {
         // Required empty public constructor
     }
 
-
+    /**
+     * Creates the view that will be shown to the user.
+     * Attaches listeners to the buttons defined in the XML.
+     * @param inflater instantiate layout XML file into its corresponding View object
+     * @param container item to contain other views
+     * @param savedInstanceState save state so we can resume later
+     * @return The view (user interface)
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
