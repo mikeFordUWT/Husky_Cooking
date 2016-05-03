411bd85b757ed98238366cb156239013c2ef8183
IanSkyles <ianskyles@gmail.com>
2016-05-03 15:26:43 -0700

    J doc addrecipefrag

--- app/src/main/java/team14/tacoma/uw/edu/husky_cooking/AddRecipeFragment.java
+++ app/src/main/java/team14/tacoma/uw/edu/husky_cooking/AddRecipeFragment.java
@@ -1,3 +1,8 @@
+/*
+ * Mike Ford and Ian Skyles
+ * TCSS450 – Spring 2016
+ * Recipe Project
+ */
 package team14.tacoma.uw.edu.husky_cooking;
 
 
@@ -13,13 +18,27 @@ import android.widget.Toast;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * This class controls the user adding recipes to our.
+ * database hosted on CSSGATE.
+ *
+ * @author Mike Ford
+ * @author Ian Skyles
+ * @version 5/2/2016
  */
 public class AddRecipeFragment extends Fragment {
+    /**
+     * the base url to add recipes to our database.
+     */
     public static final String ADD_RECIPE_URL =
             "http://cssgate.insttech.washington.edu/~_450atm14/husky_cooking/addRecipe.php?";
 
+    /**
+     * A tag for debugging.
+     */
     public static final String TAG = "AddRecipeFragment";
+    /**
+     * Edit Text for allowing entry of the recipe name, recipe description, servings, and cook time.
+     */
     private EditText mRecipeName, mRecipeDescript, mServings, mCookTime;
 
 
@@ -28,12 +47,23 @@ public class AddRecipeFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+    /**
+     * An interface which requires implementing addRecipe(String).
+     * It is used for a listener.
+     */
     public interface AddRecipeInteractionListener{
         public void addRecipe(String url);
     }
 
-
+    /**
+     * Creates the view that will be shown to the user.
+     * Attaches listeners to the buttons defined in the XML.
+     * It sets up edit text fields and a listener for add recipe button.
+     * @param inflater instantiate layout XML file into its corresponding View object
+     * @param container item to contain other views
+     * @param savedInstanceState save state so we can resume later
+     * @return The view (user interface)
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -66,7 +96,12 @@ public class AddRecipeFragment extends Fragment {
 
 
 
-
+    /**
+     * This builds a url for recipe. It includes details on name,
+     * cook time, description, and servings.
+     * @param v the view to display error toasts in
+     * @return String of the url for logging in
+     */
     private String buildRecipeUrl(View v){
         StringBuilder sb = new StringBuilder(ADD_RECIPE_URL);
 
