f4f3e456c330b0132c5b245d9c8ac6d82f5b6d76
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-05-31 13:57:56 -0700

    Got drop down menu functioning for navigation throughout the app

--- app/src/main/java/team14/tacoma/uw/edu/husky_cooking/AddIngredientFragment.java
+++ app/src/main/java/team14/tacoma/uw/edu/husky_cooking/AddIngredientFragment.java
@@ -1,9 +1,3 @@
-/*
- * Mike Ford and Ian Skyles
- * TCSS450 – Spring 2016
- * Recipe Project
- */
-
 package team14.tacoma.uw.edu.husky_cooking;
 
 
@@ -15,30 +9,16 @@ import android.view.ViewGroup;
 
 
 /**
- * This class controls the user adding ingredients to a recipe.
- * It interfaces with our database hosted on CSSGATE.
- *
- * @author Mike Ford
- * @author Ian Skyles
- * @version 5/4/2016
+ * A simple {@link Fragment} subclass.
  */
 public class AddIngredientFragment extends Fragment {
 
-    /**
-     * Required empty public constructor
-     */
+
     public AddIngredientFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Creates the view that will be shown to the user.
-     * Attaches listeners to the buttons defined in the XML.
-     * @param inflater instantiate layout XML file into its corresponding View object
-     * @param container item to contain other views
-     * @param savedInstanceState save state so we can resume later
-     * @return The view (user interface)
-     */
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
